c1b30956e487510ca0085d95c7139448f7095d4e
williambl <william@williambl.com>
2022-01-12 13:46:37 +0000

    Make em go zoom

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 
 	function createElement() {
 		const idx = Math.round(Math.random() * (possibleElements.length-1));
-		elements = [...elements, possibleElements[idx]]
+		elements = [...elements, {text: possibleElements[idx], timing: 1.2+Math.random()*7}]
 	}
 
 	function createElements() {
@@ -22,7 +22,7 @@
 <main>
 	<h1>Functional Programming</h1>
 	{#each elements as element}
-		<p class="code">{element}</p>
+		<p class="code" style="animation-duration: {element.timing}s;">{element.text}</p>
 	{/each}
 
 	<div use:inview on:enter={createElements}></div>
@@ -34,6 +34,7 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		overflow-x: hidden;
 	}
 
 	h1 {
@@ -44,11 +45,22 @@
 
 	.code {
 		font-family: "Fira Code", "Courier New", monospace;
+		position: relative;
+		animation-delay: 1.25s;
+		animation-name: zipAcross;
+		animation-fill-mode: backwards;
+		animation-timing-function: linear;
+		animation-iteration-count: infinite;
 	}
 
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
+	}
+
+	@keyframes zipAcross {
+		from { right: 100vw }
+		to { right: -100vw }
 	}
 </style>
